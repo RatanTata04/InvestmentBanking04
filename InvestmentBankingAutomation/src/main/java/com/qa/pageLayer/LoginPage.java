0e53f6fa973eead4cadef7eaf7ee1d4b5cb720f7
RatanTata04 <ratantata0131@gmail.com>
2022-08-30 12:11:06 +0530

    1st commit
    hello  guys I have created this project framework please be started with your assignments

---
+++ InvestmentBankingAutomation/src/main/java/com/qa/pageLayer/LoginPage.java
@@ -0,0 +1,11 @@
+package com.qa.pageLayer;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage {
+	
+	@FindBy(xpath = " ")
+	private WebElement username;
+
+}
